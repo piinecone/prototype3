21c945d4ad89780a3f01d1e3643e87b27c9e24aa
Nicholas Hughes <hughes.nick@gmail.com>
2013-10-18 17:39:21 -0700

    Zoom camera in and out depending on the distance of following fish in an effort to keep them visible

--- ThirdPersonCamera.cs
+++ ThirdPersonCamera.cs
@@ -1,109 +1,130 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ThirdPersonCamera : MonoBehaviour {
 
-   [SerializeField]
-   private float distanceAway;
-   [SerializeField]
-   private float distanceUp;
-   [SerializeField]
-   private float smooth;
-   [SerializeField]
-   private Transform follow;
-   [SerializeField]
-   private float widescreen = 0.2f;
-   [SerializeField]
-   private float targetingTime = 0.5f;
-   [SerializeField]
-   private TurtleState turtleState;
+  [SerializeField]
+  private float distanceAway;
+  [SerializeField]
+  private float distanceUp;
+  [SerializeField]
+  private float smooth;
+  [SerializeField]
+  private Transform follow;
+  [SerializeField]
+  private float widescreen = 0.2f;
+  [SerializeField]
+  private float targetingTime = 0.5f;
+  [SerializeField]
+  private TurtleState turtleState;
 
-   private Vector3 targetPosition;
-   private Vector3 lookDir;
-   private Vector3 velocityCamSmooth = Vector3.zero;
-   private float camSmoothDampTime = 0.1f;
-   private CamStates camState = CamStates.Behind;
+  private Vector3 targetPosition;
+  private Vector3 lookDir;
+  private Vector3 velocityCamSmooth = Vector3.zero;
+  private float camSmoothDampTime = 0.1f;
+  private CamStates camState = CamStates.Behind;
+  private List<GameObject> objectsThatShouldAlwaysBeVisible = new List<GameObject>();
+  private float maxDistanceAway;
+  private float minDistanceAway;
 
-   public enum CamStates {
-     Behind,
-     Target
-   }
+  public enum CamStates {
+    Behind,
+    Target
+  }
 
-   // Use this for initialization
-   void Start () {
-     follow = GameObject.FindWithTag("Player").transform;
-     lookDir = follow.forward;
-   }
-   
-   // Update is called once per frame
-   void Update () {
-   
-   }
+  void Start () {
+    follow = GameObject.FindWithTag("Player").transform;
+    lookDir = follow.forward;
+    maxDistanceAway = 3f * distanceAway;
+    minDistanceAway = distanceAway;
+  }
+  
+  void Update () {
+  }
 
-   void LateUpdate() {
-     distanceAway = 15;
-     distanceUp = 2;
-     //if (turtleState.isUnderwater()){
-     //  distanceAway = 15;
-     //  distanceUp = 2;
-     //} else {
-     //  distanceAway = 10;
-     //  distanceUp = 4;
-     //}
+  //if (turtleState.isUnderwater()){
+  //  distanceAway = 15;
+  //  distanceUp = 2;
+  //} else {
+  //  distanceAway = 10;
+  //  distanceUp = 4;
+  //}
 
-     Vector3 characterOffset = follow.position + new Vector3(0f, distanceUp, 0f);
+  void LateUpdate() {
+    if (objectsThatShouldAlwaysBeVisible.Count > 0){
+      bool zoomOut = false;
+      float desiredDistance = distanceAway;
+      foreach(GameObject go in objectsThatShouldAlwaysBeVisible){
+        float distance = Vector3.Distance(go.transform.position, follow.position);
+        if ((distance + 7.5f) > desiredDistance) zoomOut = true;
+      }
+      desiredDistance += zoomOut == true ? (15f * Time.deltaTime) : (-5f * Time.deltaTime);
+      distanceAway = (desiredDistance > maxDistanceAway || desiredDistance < minDistanceAway) ? distanceAway : desiredDistance;
+      //Debug.Log("Desired: " + desiredDistance + ", current: " + distanceAway);
+    }
 
-     // Determine camera state
-     if (Input.GetAxis("Fire2") > 0.01f){
-       //barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, widescreen, targetingTime);
-       camState = CamStates.Target;
-     } else {
-       //barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, 0f, targetingTime);
-       camState = CamStates.Behind;
-     }
+    Vector3 characterOffset = follow.position + new Vector3(0f, distanceUp, 0f);
 
-     switch(camState){
-       case CamStates.Behind:
-         // calculate direction from camera to player, kill y, and normalize to give a valid direction with unit magnitude
-         lookDir = characterOffset - this.transform.position;
-         lookDir.y = 0;
-         lookDir.Normalize();
-         //Debug.DrawRay(this.transform.position, lookDir, Color.green);
-         //Debug.DrawLine(follow.position, targetPosition, Color.magenta);
-         break;
-       case CamStates.Target:
-         lookDir = follow.forward;
-         break;
-     }
+    // Determine camera state
+    if (Input.GetAxis("Fire2") > 0.01f){
+      //barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, widescreen, targetingTime);
+      camState = CamStates.Target;
+    } else {
+      //barEffect.coverage = Mathf.SmoothStep(barEffect.coverage, 0f, targetingTime);
+      camState = CamStates.Behind;
+    }
 
-     targetPosition = characterOffset + follow.up * distanceUp - lookDir * distanceAway;
-     //CompensateForWalls(characterOffset, ref targetPosition);
-     smoothPosition(this.transform.position, targetPosition);
-     transform.LookAt(follow);
-   }
+    switch(camState){
+      case CamStates.Behind:
+        // calculate direction from camera to player, kill y, and normalize to give a valid direction with unit magnitude
+        lookDir = characterOffset - this.transform.position;
+        lookDir.y = 0;
+        lookDir.Normalize();
+        //Debug.DrawRay(this.transform.position, lookDir, Color.green);
+        //Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        break;
+      case CamStates.Target:
+        lookDir = follow.forward;
+        break;
+    }
 
-   private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget){
-     Debug.DrawLine(fromObject, toTarget, Color.cyan);
-     RaycastHit wallHit = new RaycastHit();
-     if (Physics.Linecast(fromObject, toTarget, out wallHit))
-     {
-       Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
-       toTarget = new Vector3(wallHit.point.x, toTarget.y, wallHit.point.z);
-     }
-   }
+    targetPosition = characterOffset + follow.up * distanceUp - lookDir * distanceAway;
+    //CompensateForWalls(characterOffset, ref targetPosition);
+    smoothPosition(this.transform.position, targetPosition);
+    transform.LookAt(follow);
+  }
 
-   private void smoothPosition(Vector3 fromPos, Vector3 toPos){
-     this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
-   }
+  private void CompensateForWalls(Vector3 fromObject, ref Vector3 toTarget){
+    Debug.DrawLine(fromObject, toTarget, Color.cyan);
+    RaycastHit wallHit = new RaycastHit();
+    if (Physics.Linecast(fromObject, toTarget, out wallHit))
+    {
+      Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
+      toTarget = new Vector3(wallHit.point.x, toTarget.y, wallHit.point.z);
+    }
+  }
+
+  private void smoothPosition(Vector3 fromPos, Vector3 toPos){
+    this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, camSmoothDampTime);
+  }
 
   public string getCamState(){
-   switch(camState){
-     case CamStates.Behind:
-       return "Behind";
-     case CamStates.Target:
-       return "Target";
-     default:
-       return "Behind";
-   }
+    switch(camState){
+      case CamStates.Behind:
+        return "Behind";
+      case CamStates.Target:
+        return "Target";
+      default:
+        return "Behind";
+    }
+  }
+
+  public void addObjectThatMustAlwaysRemainInFieldOfView(GameObject theGameObject){
+    objectsThatShouldAlwaysBeVisible.Add(theGameObject);
+  }
+
+  public void removeObjectThatMustAlwaysRemainInFieldOfView(GameObject theGameObject){
+    objectsThatShouldAlwaysBeVisible.Remove(theGameObject);
   }
 }

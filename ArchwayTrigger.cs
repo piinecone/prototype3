9e59a44347d5e8f91bae07740bb40c151c6d99b4
Nicholas Hughes <hughes.nick@gmail.com>
2014-02-08 23:27:01 -0800

    Add flash to archway upon entry

--- ArchwayTrigger.cs
+++ ArchwayTrigger.cs
@@ -5,16 +5,20 @@ public class ArchwayTrigger : MonoBehaviour {
   [SerializeField]
   private ArchwayBehavior archway;
   [SerializeField]
+  private GameObject flash;
+  [SerializeField]
   private TurtleStateController stateController;
 
   private ParticleSystem particleSystem;
 
   void Start () {
     particleSystem = GetComponent<ParticleSystem>();
+    flash.SetActive(false);
   }
 
   void OnTriggerEnter(Collider collider){
     if (collider.gameObject.tag == "Player"){
+      flash.SetActive(true);
       stateController.PlayerPassedThroughArchway(archway);
     }
   }
@@ -22,11 +26,13 @@ public class ArchwayTrigger : MonoBehaviour {
   void OnTriggerStay(Collider collider){
     particleSystem.Play();
     if (collider.gameObject.tag == "Player"){
+      flash.SetActive(false);
       stateController.PlayerPassedThroughArchway(archway);
     }
   }
 
   void OnTriggerExit(Collider collider){
+      flash.SetActive(false);
     particleSystem.Stop();
   }
 }
